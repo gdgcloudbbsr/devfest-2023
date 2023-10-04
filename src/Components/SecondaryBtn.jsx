385777f560f36204a3fcee494bcb83426c6d8df8
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2023-10-04 10:19:23 +0530

    Speakers page update

--- src/Components/SecondaryBtn.jsx
+++ src/Components/SecondaryBtn.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const SecondaryBtn = ({ text, size = false, link, className }) => {
+const SecondaryBtn = ({ text, size = false, link, className = "" }) => {
   return (
     <Link className={`SecondaryBtn ${className} ${size ? "md" : ""}`} to={link}>
       <span>{text}</span>
