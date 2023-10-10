a4178a0a57c5132b8a6ee5588281ce026c1e7d53
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2023-10-10 12:21:24 +0530

    Dashboard update and completed

--- src/Components/ProfessionalTicket.jsx
+++ src/Components/ProfessionalTicket.jsx
@@ -5,7 +5,7 @@ import PrimaryBtn from "./PrimaryBtn";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 
 const ProfessionalTicket = ({ link = "/register" }) => {
-  const stock = 0;
+  const stock = 350;
   const ticketData = data.tickets.ticketSection.options[1];
 
   const { type, description, price, button, benefits, image } = ticketData;
