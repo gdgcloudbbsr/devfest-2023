d5fa6fcf7acdc0a4aa50987637e96de4523759af
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2023-10-27 00:27:34 +0530

    uploaded

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+const App = () => {
+  return (
+    <div className={"Main"}>
+      <img src="/assets/img3.webp" alt="" className="main-bg" />
+      <div className="content">
+        <h1>Sorry!</h1>
+        <h3
+          style={{
+            backgroundColor: "var(--blue)",
+            padding: "0.5rem",
+            borderRadius: "0.5rem",
+          }}
+        >
+          Registrations closed for now!
+        </h3>
+        <h3>Will re-begin shortly!</h3>
+      </div>
+    </div>
+  );
+};
+export default App;
